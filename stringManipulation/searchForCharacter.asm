b771d46db2feff4e68f63e432cef7331365e18e1
Mohammed-Azab <mohammed3zab@gmail.com>
2024-06-18 18:06:40 +0200

    added the search For character in a string program

---
+++ stringManipulation/searchForCharacter.asm
@@ -0,0 +1,23 @@
+ORG 100H
+
+   .data segemnt
+   str DB 'exersice'
+   length DB 08H
+   numberOfOccurance DB 0
+.code segement
+    XOR DX,DX
+    XOR SI,SI
+    MOV CL, length
+
+    TheLoop:
+            MOV AL,str[SI]
+            INC SI
+            CMP AL,'e'
+            JE increment
+         contiune:
+            LOOP TheLoop
+            RET
+
+    increment:
+                INC DX
+                JMP contiune
